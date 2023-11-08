514a403408e8876f7a542a3bca80fbd5a5cd6998
Duy Nguyen <duyyb257946@gmail.com>
2023-11-08 17:14:41 +0700

    init

---
+++ netlify/edge-functions/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { handle } from "https://deno.land/x/hono/adapter/netlify/mod.ts";
+
+const app = new Hono();
+
+app.get("/country", (c) =>
+  c.json({
+    message: "Hello Netlify!",
+  })
+);
+
+export default handle(app);
